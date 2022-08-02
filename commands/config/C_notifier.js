b6185696cb2672d893535596ab586b80ab38445b
Sierra <kissellogan10@gmail.com>
2022-08-02 16:54:13 -0400

    Found right property for listing channel

--- commands/config/C_notifier.js
+++ commands/config/C_notifier.js
@@ -4,9 +4,10 @@ export default {
     data: new SlashCommandBuilder()
         .setName('notifier')
         .setDescription('Settings related to notifications.')
-        .addChannelOption(option => option.setName('channels').setDescription('Choose where notifications are posted.')),
+        .addChannelOption(option => option.setName('channel').setDescription('Choose where notifications are posted.'))
+        .addBooleanOption(option => option.setName('yt').setDescription('Toggle YouTube notifications.')),
     name: "notifier",
     async execute(interaction) {
-        console.log(interaction.options);
+        console.log(interaction.channel);
     }
 }
