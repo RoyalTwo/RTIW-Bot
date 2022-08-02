d2013bb30fcf243b4c6f8658092b85600bfa61d5
Sierra <kissellogan10@gmail.com>
2022-08-02 12:21:09 -0400

    Fixed bug where the bot never reaches notichannel's command

--- commands/config/C_notiChannel.js
+++ commands/config/C_notiChannel.js
@@ -4,8 +4,8 @@ export default {
     data: new SlashCommandBuilder()
         .setName('notichannel')
         .setDescription('Changes the notification channel to the one supplied!'),
-    name: "notiChannel",
+    name: "notichannel",
     async execute(interaction) {
-        await console.log(interaction);
+        await interaction.reply('Pong!');
     }
 }
